4171a2f72d0c9dc1995b6a1d4549e86f2f0671ea
aureopires <aureopires@DELL-Aureo.localdomain>
2026-08-10 16:28:22 +0200

    admin and fixtures category

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -16,47 +16,37 @@ class AppFixtures extends Fixture
     {
 
         CategoryFactory::createOne([
-            'name' => 'Broderie Florale',
-            'slug' => 'broderie-florale',
-            'description' => 'Motifs floraux délicats et colorés brodés à la main pour illuminer vos tenues et votre intérieur.',
-            ]);
-
-        CategoryFactory::createOne([
-            'name' => 'Personnalisation Bébé',
-            'slug' => 'personnalisation-bebe',
-            'description' => 'Articles doux et uniques pour les nouveau-nés : doudous, sorties de bain et bavoirs brodés.',
+            'name' => 'Broderie personnalisée',
+            'slug' => 'broderie-personnalisee',
+            'description' => 'Personnaliser vos vêtements accessoires et cadeaux grâce à une broderie de qualité
+            durable et élégante. Logo prénoms motifs ou texte : chaque création est réalisée avec soin.',
         ]);
 
         CategoryFactory::createOne([
-            'name' => 'Vêtements & Accessoires',
-            'slug' => 'vetements-accessoires',
-            'description' => 'Customisez vos vestes en jean, t-shirts, sacs et casquettes avec des broderies originales.',
+            'name' => 'Personnalisation textile et accessoires',
+            'slug' => 'personnalisation-textile-et-accessoires',
+            'description' => 'Je personnalise vos vêtements professionnels vos tenues d’association, équipement
+            sportif ou textiles publicitaires afin de valoriser votre image.',
         ]);
 
         CategoryFactory::createOne([
-            'name' => 'Cercles & Décoration Murale',
-            'slug' => 'cercles-decoration-murale',
-            'description' => 'Tambours de broderie artistiques prêts à être suspendus pour une déco bohème et chaleureuse.',
-            ]);
-        CategoryFactory::createOne([
-            'name' => 'Cadeaux Personnalisés',
-            'slug' => 'cadeaux-personnalises',
-            'description' => 'Offrez un cadeau unique pour un anniversaire, un mariage ou un événement spécial avec une broderie sur mesure.',
-            ]);
+            'name' => 'Couture et retouches',
+            'slug' => 'couture-et-retouches',
+            'description' => 'Besoin d’une retouche? Je réalise différents travaux de couture avec précision pour
+            donner une seconde vie à vos textiles ou créer une pièce unique.',
+        ]);
 
         CategoryFactory::createOne([
-            'name' => 'Linge de Maison',
-            'slug' => 'linge-de-maison',
-            'description' => 'Serviettes, torchons, taies d’oreiller et nappes élégamment personnalisés au fil.',
-            ]);
-
-        CategoryFactory::createMany(4);
-
+            'name' => 'Création sur mesure',
+            'slug' => 'creation-sur-mesure',
+            'description' => 'Envie d’un cadeau personnalisé ou d’une création unique ? ensemble nous imaginons un
+            projet qui vous ressemble adapté à vos envies et à votre budget.',
+        ]);
 
         UserFactory::createOne([
             'firstName' => 'Admin',
             'lastName' => 'Admin',
-            'email' =>  'admin@broderie.com',
+            'email' => 'admin@broderie.com',
             'roles' => ['ROLE_ADMIN'],
             'password' => 'admin123'
         ]);
